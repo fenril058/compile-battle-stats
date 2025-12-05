d4e0fe9a11e7a6cb78af22a4bc47416c0284379f
ril <fenril.nh@gmail.com>
2025-12-05 13:09:57 +0900

    introduce test environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  test: {
+    environment: "jsdom", // DOM環境が必要なテストのためにjsdomを使用
+    setupFiles: ["./src/setupTests.ts"], // グローバルな設定ファイル
+    globals: true, // describe, test, expectなどをグローバルにする
+  },
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
